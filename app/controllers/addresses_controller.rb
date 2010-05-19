48171b5f37e93a5300da2b10d9f64cf96387fcaf
Etienne van Tonder <etiennevt@gmail.com>
2010-05-19 18:30:38 +1000

    Update some comments

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,10 @@ class AddressesController < Spree::BaseController
   belongs_to :user
   actions :index, :update
 
+  # Include helper to that we can use field_container helper which
+  # is an admin helper
   helper 'admin/base'
-  
+
+  # Include shared code
   include DefaultAddresses
 end
--- app/controllers/admin/addresses_controller.rb
+++ app/controllers/admin/addresses_controller.rb
@@ -3,5 +3,6 @@ class Admin::AddressesController < Admin::BaseController
   belongs_to :user
   actions :index, :update
 
+  # Include shared code
   include DefaultAddresses
 end
