2e39653239e934067b2860bb3ab055f7896cf3fb
Etienne van Tonder <etiennevt@gmail.com>
2010-05-20 16:45:13 +1000

    Fix bug in observer code

--- app/models/order_transaction_observer.rb
+++ app/models/order_transaction_observer.rb
@@ -4,10 +4,10 @@ class OrderTransactionObserver < ActiveRecord::Observer
   # Generic transition callback *after* the transition is performed
   def after_transition(order, attribute_name, event_name, from_state, to_state)
     if event_name.to_s == "complete"
-      if order.bill_address_id && order.ship_address_id
+      if order.bill_address.id && order.ship_address.id
         order.user.update_attributes!(
-          :bill_address_id => order.bill_address_id,
-          :ship_address_id => order.ship_address_id
+          :bill_address_id => order.bill_address.id,
+          :ship_address_id => order.ship_address.id
         )
       end
     end
