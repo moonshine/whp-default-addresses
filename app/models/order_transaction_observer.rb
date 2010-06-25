869b7cac0ea181e91de3a4366a7033261aa03a24
Etienne van Tonder <etiennevt@gmail.com>
2010-06-25 15:51:03 +1000

    Update state_transition observer method to use the new format

--- app/models/order_transaction_observer.rb
+++ app/models/order_transaction_observer.rb
@@ -2,8 +2,8 @@ class OrderTransactionObserver < ActiveRecord::Observer
   observe :order
 
   # Generic transition callback *after* the transition is performed
-  def after_transition(order, attribute_name, event_name, from_state, to_state)
-    if event_name.to_s == "complete"
+  def after_transition(order, transition)
+    if transition.attributes[:event].to_s == "complete"
       if order.bill_address.id && order.ship_address.id
         order.user.update_attributes!(
           :bill_address_id => order.bill_address.id,
