3dd30ea43c539f1e7db149092d3aad67361644ab
Dominik Masur <dominik.masur@googlemail.com>
2010-12-02 13:12:52 +0100

    require modul

--- lib/whp_default_addresses.rb
+++ lib/whp_default_addresses.rb
@@ -10,6 +10,7 @@ module WhpDefaultAddresses
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+require 'default_addresses'
 
       Address.class_eval do
         belongs_to :user
